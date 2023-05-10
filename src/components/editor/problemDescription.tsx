32cd25eed7dd6e5c55000437405d00674cf863d3
Luka Stojić <124006893+stojic-luka@users.noreply.github.com>
2023-05-10 12:24:17 +0200

    Update problemDescription.tsx

--- src/components/editor/problemDescription.tsx
+++ src/components/editor/problemDescription.tsx
@@ -1,1 +1,19 @@
+import { Container } from "@mui/material";
 
+function NavBar() {
+  return (
+    <Container maxWidth="lg">
+      <h1>a</h1>
+    </Container>
+  );
+}
+
+function NavBarEditor() {
+  return (
+    <Container maxWidth="lg">
+      <h1>a</h1>
+    </Container>
+  );
+}
+
+export { NavBar, NavBarEditor };
